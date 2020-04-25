de4f9f6d3616d9638d3f0f778fca614d9cf984a0
RafaArturo <draco032@gmail.com>
2020-04-25 15:06:47 -0400

    Add alternative newtask and default options

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-#ToDO List
-A Todo List web application developed in javascript, html and CSS.
+# ToDO List
+A Todo List web application developed in javascript, html and CSS. This app is made in two differents ways at differents files: main1.js, and main2.js
+
+## main1.js
+This file was made with old method that changes the html, an reload all the document with each little .
+
+## main2.js
+In this case, the app have a javascript oriented method. Is more efficient than the other method, and the base of the modern way.
+
+## How to use?
+You Only need change the script reference at index.html, line 9.
+<script type="text/javascript" src= *HERE* ></script>
--- main2.js
+++ main2.js
@@ -1,5 +1,9 @@
+var tasks = [];
+var id = 0;
+
 function getLi() {
     var li = document.createElement("li");
+    li.className = "tasks";
     return li;
 }
 
@@ -11,15 +15,32 @@ function getCheckBox() {
 
 function getPriority() {
     priority = document.createElement("select");
-    priority.className = 'priority'
+    priority.className = 'priority';
     return priority;
 }
 
-function getDeleteButton(){
+function deleteTask() {
+    var taskslist = document.getElementsByClassName('tasks');
+    for (var i = 0; i < taskslist.length; i++) {
+        if (taskslist[i].id === id) {
+            taskslist[i].remove();
+            break;
+        }
+    };
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === id) {
+            tasks.splice(i,1);
+        };
+    }
+}
+
+function getDeleteButton(id){
     var deleteButton = document.createElement("input");
     deleteButton.type = "button";
     deleteButton.value = "Delete";
     deleteButton.className = "delete";
+    deleteButton.id = id;
+    deleteButton.onclick = deleteTask();
     return deleteButton;
 }
 
@@ -27,7 +48,7 @@ function getComponent(taskName) {
     var li = getLi();
     var checkBox = getCheckBox();
     var priority = getPriority();
-    var deleteButton = getDeleteButton();
+    var deleteButton = getDeleteButton(id);
     li.append(checkBox);
     li.append(taskName);
     li.append(priority);
@@ -44,9 +65,23 @@ function handleEnter(event) {
 function newTask() {
     var input = document.getElementById('input');
     if (input.value.trim().length > 0) {
-        var tasks = document.getElementById('task-list');
+        var taskslist = document.getElementById('task-list');
         var newTask = getComponent(input.value);
-        tasks.appendChild(newTask);        
+        taskslist.appendChild(newTask);
+        tasks.push({ id:id++, title: input.value, checked: false, priority: 'medium' });
     };
     input.value = '';
+}
+
+function printTaskList(tasks){
+    if (tasks.length === 0) {
+        var ul = document.getElementById("task-list");
+        var msg = document.getElementsByClassName("alertText");
+        if (msg.length === 0) {
+            msg = document.createElement("p");
+            msg.className = "alertText";
+            msg.innerText = "Nothing to do? Start to add!!";
+            ul.appendChild(msg);            
+        }
+    }
 }
